6c91ec80a8fff5db3123116e1a81fb5721243a6d
Yulia <juliam2007@mail.ru>
2018-01-26 21:07:39 +0300

    Add file map.js

---
+++ js/map.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var COUNT_USERS = 8;
+var MIN_GUEST = 1;
+var MAX_GUEST = 20;
+var MIN_ROOMS = 1;
+var MAX_ROOMS = 5;
+var MIN_PRICE = 1000;
+var MAX_PRICE = 1000000;
+var PIN_HEIGHT = 75;
+var PIN_WIDTH = 56;
+
+var TITLE_ADS = ['Большая уютная квартира','Маленькая неуютная квартира','Огромный прекрасный дворец','Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+var TYPE_OF_ROOMS = ['flat', 'house', 'bungalo'];
+var TIME = ['12:00', '13:00', '14:00'];
+var FACILITY = ['wifi', 'dishwasher', 'parking', 'elevator', 'conditioner'];
