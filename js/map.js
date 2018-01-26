5ea6e9d80872e0be4ad1554a8087b069217afc39
Yulia <juliam2007@mail.ru>
2018-01-26 22:02:54 +0300

    Add function generateAds

--- js/map.js
+++ js/map.js
@@ -15,6 +15,43 @@ var TYPE_OF_ROOMS = ['flat', 'house', 'bungalo'];
 var TIME = ['12:00', '13:00', '14:00'];
 var FACILITY = ['wifi', 'dishwasher', 'parking', 'elevator', 'conditioner'];
 
+// Функция, возвращающаая массив объектов объявлений
+function generateAds() {
+  var ads = [];
+  var locationX = getRandomNumber(300, 900);
+  var locationY = getRandomNumber(100, 500);
+  var userAvatars = shuffleArray(generateAvatars());
+  var adHeadlines = shuffleArray(TITLE_ADS);
+
+  for (var i = 0; i < COUNT_USERS; i++) {
+    ads.push({
+      'author': {
+        'avatar':  userAvatars[i]// Перебираем массив и ставим первое значение обновленного массива
+      },
+      'offer': {
+        'title': adHeadlines[i], // Перебираем массив и ставим первое значение обновленного массива
+        'adress': (locationX + ', ' + locationY),
+        'price': getRandomNumber(MIN_PRICE, MAX_PRICE),  // число, случайная цена от 1000 до 1 000 000
+        'type': getRandomElement(TYPE_OF_ROOMS), // Выбираем случайное число из массива типа комнат
+        'rooms': getRandomNumber(MIN_ROOMS, MAX_ROOMS), // Выбираем случайное число из массива количества комнат
+        'guests': getRandomNumber(MIN_GUEST, MAX_GUEST), // число, случайное количество гостей, которое можно разместить
+        'checkin': getRandomElement(TIME),  // Выбираем случайное число из массива времени заезда
+        'checkout': getRandomElement(TIME), // Выбираем случайное число из массива времени выезда
+        'features': getArrayLength(FACILITY), // Выбираем случайное число из массива удобств
+        'description': '',
+        'photos': []
+      },
+      'location': {
+        'x': locationX, // случайное число, координата x метки на карте в блоке .tokyo__pin-map от 300 до 900,
+        'y': locationY // случайное число, координата y метки на карте в блоке .tokyo__pin-map от 100 до 500
+      }
+    });
+  }
+  return ads;
+}
+
+console.log(generateAds());
+
 // Генерируем массив аватарок
 function generateAvatars() {
   var listAvatars = [];
