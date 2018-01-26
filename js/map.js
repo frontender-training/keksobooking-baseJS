4d4152e7be7119c9ce8f769d1ad16d70f65378fe
Yulia <juliam2007@mail.ru>
2018-01-26 21:54:59 +0300

    Add function generateAvatars

--- js/map.js
+++ js/map.js
@@ -10,7 +10,21 @@ var MAX_PRICE = 1000000;
 var PIN_HEIGHT = 75;
 var PIN_WIDTH = 56;
 
-var TITLE_ADS = ['Большая уютная квартира','Маленькая неуютная квартира','Огромный прекрасный дворец','Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+var TITLE_ADS = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
 var TYPE_OF_ROOMS = ['flat', 'house', 'bungalo'];
 var TIME = ['12:00', '13:00', '14:00'];
 var FACILITY = ['wifi', 'dishwasher', 'parking', 'elevator', 'conditioner'];
+
+// Генерируем массив аватарок
+function generateAvatars() {
+  var listAvatars = [];
+  for (var i=1; i < COUNT_USERS + 1; i++) {
+    if (i < 10) {
+      var Avatars = 'img/avatars/user' + '0' + i + '.png';
+    } else {
+      var Avatars = 'img/avatars/user' + i + '.png';
+    }
+    listAvatars.push(Avatars);
+  }
+  return listAvatars;
+}
