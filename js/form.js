7a0f7f438b1e6ece4ca7ecb10a32e5686a26959e
cybersunt <juliam2007@mail.ru>
2018-03-28 14:34:05 +0300

    Add synchronization of form fields

--- js/form.js
+++ js/form.js
@@ -4,12 +4,47 @@ var uploadOffer = document.querySelector('.notice__form');
 var uploadOfferTitle = uploadOffer.querySelector('#title');
 var uploadOfferPrice = uploadOffer.querySelector('#price');
 
+var uploadOfferTimeIn = uploadOffer.querySelector('#time');
+var uploadOfferTimeOut = uploadOffer.querySelector('#timeout');
+var uploadOfferTypeAccomond = uploadOffer.querySelector('#type');
+var uploadOfferRoomNumber = uploadOffer.querySelector('#room_number');
+var uploadOfferSeatsNumber = uploadOffer.querySelector('#capacity');
+
 // Добавление обработчиков валидации формы
 uploadOfferTitle.addEventListener('invalid', validationTitle);
 uploadOfferPrice.addEventListener('invalid', validationPrice);
 
+// Добавление обработчиков синхронизации полей формы
+uploadOfferTimeIn.addEventListener('change', onChangeTime);
+uploadOfferTypeAccomond.addEventListener('change', onChangePrice);
+uploadOfferRoomNumber.addEventListener('change', onChangeRooms);
+
 // Добавление обработчика валидации формы
 // uploadOfferComment.addEventListener('input', showError);
+
+function onChangeRooms(evt) {
+  var element = evt.target.value;
+  if (element === '1 комната') {
+    uploadOfferSeatsNumber.value = 'не для гостей';
+  } else if (element !== '1 комната') {
+    uploadOfferSeatsNumber.value = 'для 3 гостей';
+  }
+}
+
+function onChangePrice(evt) {
+  var element = evt.target.value;
+  if (element === 'flat') {
+    uploadOfferPrice.value = 1000;
+  } else if (element === 'hovel') {
+    uploadOfferPrice.value = 0;
+  } else if (element === 'palace') {
+    uploadOfferPrice.value = 10000;
+  }
+}
+
+function onChangeTime(evt) {
+  uploadOfferTimeOut.value = evt.target.value;
+}
 
 function validationPrice(evt) {
   var element = evt.target;
@@ -24,7 +59,6 @@ function validationPrice(evt) {
   }
 }
 
-// Валидация формы
 function validationTitle(evt) {
   var element = evt.target;
   if (element.validity.tooShort) {
